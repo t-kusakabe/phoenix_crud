9caa54bdb89e311a48a41741de3e19d0f57a3c35
t-kusakabe <ttmac0704@gmail.com>
2018-07-29 20:37:04 +0900

    Add: Post#create action

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -6,5 +6,19 @@ defmodule PhoenixCrud.PostController do
     changeset = Post.changeset(%Post{})
     render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"post" => post_params}) do
+    changeset = Post.changeset(%Post{}, post_params)
+    case Repo.insert(changeset) do 
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "投稿しました")
+        |> redirect(to: post_path(conn, :new))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "投稿に失敗しました")
+        |> render("new.html", changeset: changeset)
+    end
+  end
 end
 
